50b81579ae9973373b54ee54d373a27c48d5f63d
corbin murray <corbin.murray63@yahoo.com>
2024-11-18 11:31:50 -0600

    Add jsdoc

--- components/Maze.tsx
+++ components/Maze.tsx
@@ -2,12 +2,21 @@
 import * as d3 from "d3";
 import { useEffect, useRef } from "react";
 
-const Maze = ({ columns, rows, cellSize }: { columns: number; rows: number; cellSize: number }) => {
+/**
+ * Component for generating a maze, displaying the maze, and displaying the A* algorithm in real-time
+ *
+ * @component
+ * @example
+ * return (
+ * 	<Maze columns={10} rows ={10} cellSizeInPx={50} />
+ * )
+ */
+const Maze = ({ columns, rows, cellSizeInPx }: { columns: number; rows: number; cellSizeInPx: number }) => {
 	const svgRef = useRef<SVGSVGElement | null>(null);
 
 	useEffect(() => {
-		const height = rows * cellSize;
-		const width = rows * cellSize;
+		const height = rows * cellSizeInPx;
+		const width = rows * cellSizeInPx;
 
 		const svg = d3.select(svgRef.current).attr("width", width).attr("height", height).attr("class", "");
 
@@ -15,7 +24,7 @@ const Maze = ({ columns, rows, cellSize }: { columns: number; rows: number; cell
 		const gridData = [];
 		for (let row = 0; row < rows; row++) {
 			for (let col = 0; col < columns; col++) {
-				gridData.push({ x: col * cellSize, y: row * cellSize });
+				gridData.push({ x: col * cellSizeInPx, y: row * cellSizeInPx });
 			}
 		}
 
@@ -26,11 +35,13 @@ const Maze = ({ columns, rows, cellSize }: { columns: number; rows: number; cell
 			.join("rect")
 			.attr("x", (d) => d.x)
 			.attr("y", (d) => d.y)
-			.attr("width", cellSize)
-			.attr("height", cellSize)
-			.attr("class", "fill-primary stroke-primary-content cursor-pointer hover:fill-secondary hover:stroke-secondary-content transition-[fill] duration-100 ease-in-out");
-
-	}, [columns, rows, cellSize]);
+			.attr("width", cellSizeInPx)
+			.attr("height", cellSizeInPx)
+			.attr(
+				"class",
+				"fill-primary stroke-primary-content cursor-pointer hover:fill-secondary hover:stroke-secondary-content transition-[fill] duration-100 ease-in-out"
+			);
+	}, [columns, rows, cellSizeInPx]);
 
 	return <svg ref={svgRef}></svg>;
 };
